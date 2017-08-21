5043226709c5c9b4e006d065143148a27828fa90
Chris Zuber <shgysk8zer0@gmail.com>
2017-08-21 11:44:08 -0700

    Add testing script

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+namespace shgysk8zer0\SchemaServer;
+
+if (PHP_SAPI === 'cli') {
+	set_exception_handler(function(\Throwable $e)
+	{
+		echo json_encode([
+			'message' => $e->getMessage(),
+			'line'    => $e->getLine(),
+			'file'    => $e->getFile(),
+		], JSON_PRETTY_PRINT);
+	});
+	set_include_path(dirname(__DIR__, 2));
+	spl_autoload_register('spl_autoload');
+
+	$me = new Person();
+	$me->givenName ='Christopher';
+	$me->additionalName = 'Wayne';
+	$me->familyName = 'Zuber';
+	$me->image = new ImageObject();
+	$me->image->width = 128;
+	$me->image->height = 128;
+	$me->image->url = sprintf(
+		'https://gravatar.com/avatar/%s?s=%d',
+		md5('chris@chriszuber.com'),
+		$me->image->width
+	);
+	$me->image->caption = "{$me->givenName} {$me->familyName} (Gravatar)";
+
+	echo $me . PHP_EOL;
+
+	// echo json_encode($me, JSON_PRETTY_PRINT) . PHP_EOL;
+}
