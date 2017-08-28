6704d9ca1ca233bd0e10dc8d46bd92178e96a922
Chris Zuber <shgysk8zer0@gmail.com>
2017-08-28 11:07:31 -0700

    Add BreadCrumbList and List-type classes

---
+++ listitem.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author Chris Zuber <chris@chriszuber.com>
+ * @package superuserdev/schemaserver
+ * @copyright 2017
+ * @license https://opensource.org/licenses/LGPL-3.0 GNU Lesser General Public License version 3
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+namespace SuperUserDev\SchemaServer;
+/**
+ * @see https://schema.org/ListItem
+ */
+class ListItem extends Instangible
+{
+	final public function setItem(String $item)
+	{
+		$this->_set('item', $item);
+	}
+
+	final public function setNextItem(ListItem $item)
+	{
+		$this->_set('nextItem', $item);
+	}
+
+	final public function setPosition(Int $position)
+	{
+		$this->_set('position', $position);
+	}
+
+	final public function setPreviousItem(ListItem $item)
+	{
+		$this->_set('previousItem', $item);
+	}
+}
