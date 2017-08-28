c9696b7c60e54e2c18ae277bbb1160392de7f82a
Chris Zuber <shgysk8zer0@gmail.com>
2017-08-28 01:05:31 -0700

    Add Action and all required classes Add Action class and all classes required to set various properties (excluding DataFeed)

---
+++ enumeration.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author Chris Zuber <chris@chriszuber.com>
+ * @package superuserdev/schemaserver
+ * @copyright 2017
+ * @license https://opensource.org/licenses/LGPL-3.0 GNU Lesser General Public License version 3
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+namespace SuperUserDev\SchemaServer;
+
+/**
+ * @see https://schema.org/Enumeration
+ */
+class Enumeration extends Intangible
+{
+	final public function setSupersededBy(Thing $thing)
+	{
+		if ($thing instanceof Enumeration or $thing instanceof Property) {
+			$this->_set('supersededBy', $thing);
+		} else {
+			throw new \InvalidArgumentException(sprintf(
+				'Expected an instance of Enumeration or Property. Instance of %s given',
+				$thing::getType()
+			));
+		}
+	}
+}
