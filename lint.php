41c10e3000f88fc17fc4e6bb0b0efefedd22b69b
Chris Zuber <shgysk8zer0@gmail.com>
2017-08-29 17:16:48 -0700

    Exit with 400 for linting if not CLI

--- lint.php
+++ lint.php
@@ -4,6 +4,10 @@ const EXTS = ['php'];
 const IGNORED_DIRS = ['.git'];
 
 if (PHP_SAPI === 'cli') {
+	/**
+	 * @param  String $dir The directory to lint
+	 * @return Bool        Whether or not linting all PHP scripts was successful
+	 */
 	function lint_dir(String $dir = __DIR__): Bool
 	{
 		$paths = new \DirectoryIterator($dir);
@@ -29,17 +33,22 @@ if (PHP_SAPI === 'cli') {
 		return $valid;
 	}
 
+	/**
+	 * @param  String $path  The file to lint
+	 * @return Bool          If script passed linting test
+	 */
 	function lint_file(String $path): Bool
 	{
 		$path = escapeshellarg($path);
 		$msg = exec("php -l {$path}", $arr, $return_var);
 		if ($return_var !== 0) {
-			throw new \Exception($msg);
+			echo $msg . PHP_EOL;
+			return false;
 		} else {
 			return true;
 		}
 	}
-	lint_dir(__DIR__);
+	exit(lint_dir(__DIR__) ? 0 : 1);
 } else {
 	http_response_code(400);
 }
