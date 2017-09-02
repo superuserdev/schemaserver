9865a751a9a67e6235e2ca927e12b425b4b2f701
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-02 12:59:50 -0700

    Add all properties for THing

--- db.sql
+++ db.sql
@@ -14,21 +14,47 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE IF EXISTS "schema";
+--
+-- Name: schema; Type: DATABASE; Schema: -; Owner: -
+--
+
+CREATE DATABASE "schema" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+\connect "schema"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: SCHEMA "public"; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
 --
 
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS "plpgsql" WITH SCHEMA "pg_catalog";
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+-- Name: EXTENSION "plpgsql"; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+COMMENT ON EXTENSION "plpgsql" IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
+SET search_path = "public", pg_catalog;
 
 SET default_tablespace = '';
 
@@ -38,13 +64,16 @@ SET default_with_oids = false;
 -- Name: thing; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE thing (
-    identifier text NOT NULL PRIMARY KEY,
-    name text,
-    image jsonb,
-    description text,
-    url text,
-    sameas text
+CREATE TABLE "thing" (
+    "identifier" "text" NOT NULL,
+    "name" "text",
+    "image" "jsonb",
+    "description" "text",
+    "url" "text",
+    "sameas" "text",
+    "alternatename" "text",
+    "disambiguatingdescription" "text",
+    "mainentityofpage" "jsonb"
 );
 
 
@@ -52,111 +81,121 @@ CREATE TABLE thing (
 -- Name: intangible; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE intangible (
+CREATE TABLE "intangible" (
 )
-INHERITS (thing);
+INHERITS ("thing");
 
 
 --
 -- Name: structuredvalue; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE structuredvalue (
+CREATE TABLE "structuredvalue" (
 )
-INHERITS (intangible);
+INHERITS ("intangible");
 
 
 --
 -- Name: contactpoint; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE contactpoint (
-    contacttype text
+CREATE TABLE "contactpoint" (
+    "contacttype" "text"
 )
-INHERITS (structuredvalue);
+INHERITS ("structuredvalue");
 
 
 --
 -- Name: creativework; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE creativework (
-    author jsonb
+CREATE TABLE "creativework" (
+    "author" "jsonb"
 )
-INHERITS (thing);
+INHERITS ("thing");
 
 
 --
 -- Name: mediaobject; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE mediaobject (
-    width integer,
-    height integer,
-    uploaddate date
+CREATE TABLE "mediaobject" (
+    "width" integer,
+    "height" integer,
+    "uploaddate" "date"
 )
-INHERITS (creativework);
+INHERITS ("creativework");
 
 
 --
 -- Name: imageobject; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE imageobject (
-    caption text,
-    thumbnail jsonb
+CREATE TABLE "imageobject" (
+    "caption" "text",
+    "thumbnail" "jsonb"
 )
-INHERITS (mediaobject);
+INHERITS ("mediaobject");
 
 
 --
 -- Name: organization; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE organization (
-    address jsonb,
-    logo jsonb,
-    email text,
-    telephone text,
-    faxnumber text
+CREATE TABLE "organization" (
+    "address" "jsonb",
+    "logo" "jsonb",
+    "email" "text",
+    "telephone" "text",
+    "faxnumber" "text"
 )
-INHERITS (thing);
+INHERITS ("thing");
 
 
 --
 -- Name: person; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE person (
-    givenname text,
-    additionalname text,
-    familyname text,
-    address jsonb,
-    worksfor jsonb,
-    email text,
-    jobtitle text
+CREATE TABLE "person" (
+    "givenname" "text",
+    "additionalname" "text",
+    "familyname" "text",
+    "address" "jsonb",
+    "worksfor" "jsonb",
+    "email" "text",
+    "jobtitle" "text"
 )
-INHERITS (thing);
+INHERITS ("thing");
 
 
 --
 -- Name: postaladdress; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE postaladdress (
-    addresscountry text,
-    addresslocality text,
-    addressregion text,
-    postofficeboxnumber integer,
-    streetaddress text,
-    postalcode integer
+CREATE TABLE "postaladdress" (
+    "addresscountry" "text",
+    "addresslocality" "text",
+    "addressregion" "text",
+    "postofficeboxnumber" integer,
+    "streetaddress" "text",
+    "postalcode" integer
 )
-INHERITS (contactpoint);
+INHERITS ("contactpoint");
 
 
 --
 -- Name: thing thing_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
+
+ALTER TABLE ONLY "thing"
+    ADD CONSTRAINT "thing_pkey" PRIMARY KEY ("identifier");
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: -
+--
+
+GRANT ALL ON SCHEMA "public" TO PUBLIC;
 
 
 --
