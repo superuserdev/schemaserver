ee72d9011968643b61505e63b0d224cef4780001
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-12 21:21:58 -0700

    Add PHP script to store and share constants

---
+++ .tests/consts.php
@@ -0,0 +1,8 @@
+<?php
+namespace SuperUserDev\SchemaServer\Tests;
+const CREDS           = '../creds.json';
+const MIN_PHP_VERSION = '7.1';
+const DB_TEST         = true;
+const EXTS            = ['php'];
+const IGNORED_DIRS    = ['.git', '.tests'];
+const TZONE           = 'America/Los_Angeles';
